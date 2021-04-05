dfe17269ac9f2c98d791e96a790a90483fd88516
pedanticdan <78196300+pedanticdan@users.noreply.github.com>
2021-04-05 16:15:37 -0600

    obs-placeholder: increase version number

--- src/obs-placeholder.h
+++ src/obs-placeholder.h
@@ -6,6 +6,6 @@ obs-placeholder
 
 #include <obs.hpp>
 
-#define OBS_PLACEHOLDER_VERSION "0.0.1"
+#define OBS_PLACEHOLDER_VERSION "0.0.2"
 
 #define blog(level, msg, ...) blog(level, "[obs-placeholder] " msg, ##__VA_ARGS__)
