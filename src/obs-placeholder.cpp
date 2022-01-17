31d441890e86597d158985ca8d9ce9f514ff7b93
pedanticdan <78196300+pedanticdan@users.noreply.github.com>
2022-01-17 16:39:47 -0700

    Automaticallt change placaholder.png when user switches profiles

--- src/obs-placeholder.cpp
+++ src/obs-placeholder.cpp
@@ -37,6 +37,41 @@ OBS_DECLARE_MODULE()
 /* Implements common ini-based locale (optional) */
 OBS_MODULE_USE_DEFAULT_LOCALE("obs-placeholder", "en-US")
 
+static void profile_changed(enum obs_frontend_event event,
+  void *) {
+  if (event == OBS_FRONTEND_EVENT_PROFILE_CHANGED) {
+    blog(LOG_INFO, "obs-placeholder OBS_FRONTEND_EVENT_PROFILE_CHANGED");
+    config_t *obsConfig = obs_frontend_get_profile_config();
+
+    const char *png = config_get_string(obsConfig, SECTION_NAME, PARAM_PNG);
+    blog(LOG_INFO, "obs-placeholder path to custom png '%s'", png);
+    char *custom = os_get_config_path_ptr(PLACEHOLDER_DIR);
+    blog(LOG_INFO, "obs-placeholder path to placeholder dir '%s'", custom);
+
+    int ret;
+    if (!os_file_exists(custom))
+      os_mkdirs(custom);
+    bfree(custom);
+    blog(LOG_INFO, "obs-placeholder make sure dir exists [%s]", custom);
+    custom = os_get_config_path_ptr(PLACEHOLDER_PNG);
+    blog(LOG_INFO, "obs-placeholder get file path [%s]", custom);
+    if (png != NULL && os_file_exists(png)) {
+      blog(LOG_INFO, "obs-placeholder exists [%s]", png);
+      if (os_file_exists(custom)) {
+        blog(LOG_INFO, "obs-placeholder exists [%s]", custom);
+        ret = os_unlink(custom);
+      }
+      blog(LOG_INFO, "obs-placeholder copy [%s] to [%s]", png, custom);
+      ret = os_copyfile(png, custom);
+    } else {
+      blog(LOG_INFO, "obs-placeholder does not exist or not configured");
+      blog(LOG_INFO, "obs-placeholder unlink [%s]", custom);
+      ret = os_unlink(custom);
+    }
+    bfree(custom);
+  }
+}
+
 bool obs_module_load(void) {
   blog(LOG_INFO, "entering obs_module_load");
   obs_data_t *obsData = obs_get_private_data();
@@ -52,6 +87,8 @@ bool obs_module_load(void) {
         (QAction *)obs_frontend_add_tools_menu_qaction(menuActionText);
     QObject::connect(menuAction, &QAction::triggered,
                    [settingsDialog] { settingsDialog->show(); });
+    obs_frontend_add_event_callback(profile_changed, nullptr);
+
   }
   blog(LOG_INFO, "exiting obs_module_load");
   return true;
--- src/obs-placeholder.h
+++ src/obs-placeholder.h
@@ -24,7 +24,7 @@ obs-placeholder
 
 #include <obs.hpp>
 
-#define OBS_PLACEHOLDER_VERSION "1.0.0"
+#define OBS_PLACEHOLDER_VERSION "1.1.0"
 
 #define blog(level, msg, ...)                                                  \
   blog(level, "[obs-placeholder] " msg, ##__VA_ARGS__)
